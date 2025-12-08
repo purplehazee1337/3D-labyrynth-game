a87888c982288651ec31a360c569e62bb5af9519
Purplehazee1337 <wjechowski@gmail.com>
2025-12-08 12:07:59 +0100

    Separated player class

--- classes/player.js
+++ classes/player.js
@@ -1,9 +1,120 @@
+const deg = Math.PI / 180;
+const world = document.getElementById("world");
+
 export default class Player {
-  constructor(x, y, z, rx, ry) {
+  constructor(x, y, z, rx, ry, lock = true) {
     this.x = x;
     this.y = y;
     this.z = z;
     this.rx = rx;
     this.ry = ry;
+    this.lock = lock;
+
+    this.pressLeft = 0;
+    this.pressRight = 0;
+    this.pressForward = 0;
+    this.pressBack = 0;
+    this.pressUp = 0;
+    this.mouseX = 0;
+    this.mouseY = 0;
+
+    //if the key is pressed
+    document.addEventListener("keydown", (e) => {
+      console.log("clicked", e.key);
+      switch (e.key) {
+        case "w":
+        case "ArrowUp":
+          if (!this.lock) this.pressForward = 1;
+          break;
+        case "s":
+        case "ArrowDown":
+          if (!this.lock) this.pressBack = 1;
+          break;
+        case "d":
+        case "ArrowRight":
+          if (!this.lock) this.pressRight = 1;
+          break;
+        case "a":
+        case "ArrowLeft":
+          if (!this.lock) this.pressLeft = 1;
+          break;
+        case " ":
+          if (!this.lock) this.pressUp = 1;
+          break;
+        default:
+      }
+    });
+
+    //if the key is released
+    document.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "w":
+        case "ArrowUp":
+          this.pressForward = 0;
+          break;
+        case "s":
+        case "ArrowDown":
+          this.pressBack = 0;
+          break;
+        case "d":
+        case "ArrowRight":
+          this.pressRight = 0;
+          break;
+        case "a":
+        case "ArrowLeft":
+          this.pressLeft = 0;
+          break;
+        case " ":
+          this.pressUp = 0;
+          break;
+        default:
+      }
+    });
+
+    //if the mouse is moved
+    document.addEventListener("mousemove", (e) => {
+      this.mouseX = e.movementX;
+      this.mouseY = e.movementY;
+    });
+  }
+
+  changeLock() {
+    this.lock = !this.lock;
+  }
+
+  updateMovement() {
+    //count movement
+    const dx =
+      Math.cos(this.ry * deg) * (this.pressRight - this.pressLeft) -
+      Math.sin(this.ry * deg) * (this.pressForward - this.pressBack) * 2;
+
+    const dz =
+      -(
+        Math.sin(this.ry * deg) * (this.pressRight - this.pressLeft) +
+        Math.cos(this.ry * deg) * (this.pressForward - this.pressBack)
+      ) * 2;
+
+    const dy = -this.pressUp * 5;
+    const drx = this.mouseY / 4;
+    const dry = -this.mouseX / 4;
+
+    //add movement to the coordinates
+    this.x = this.x + dx;
+    this.y = Math.max(Math.min(this.y + dy + 2, 0), -150);
+    this.z = this.z + dz;
+    if (!this.lock) {
+      this.rx = this.rx + drx;
+      this.ry = this.ry + dry;
+    }
+
+    //change coordinate of the world
+    world.style.transform =
+      `translateZ(600px) ` +
+      `rotateX(${-this.rx}deg) ` +
+      `rotateY(${-this.ry}deg) ` +
+      `translate3d(${-this.x}px,${-this.y}px,${-this.z}px)`;
+
+    this.mouseX = 0;
+    this.mouseY = 0;
   }
 }
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import { labirynth } from "./utils/labirynth.js";
-import { canLock } from "./utils/menu.js";
 import Player from "./classes/player.js";
+import { canLock } from "./utils/menu.js";
 
-//world constant
-const deg = Math.PI / 180;
+const container = document.getElementById("container");
+
+container.onclick = () => {
+  if (canLock) container.requestPointerLock();
+};
+
+document.addEventListener("pointerlockchange", (e) => {
+  pawn.changeLock();
+});
+
 const pawn = new Player(0, 0, 0, 0, 0);
 const world = document.getElementById("world");
-const container = document.getElementById("container");
+world.style.backgroundImage = "background.webp";
 
 //Objects data
 // x y z rx ry rz w h c
@@ -28,83 +36,6 @@ const coins = [
 
 const keys = [[-800, 30, 400, 0, 0, 0, 50, 50, "grey"]];
 
-//variables for movement
-let pressLeft = 0;
-let pressRight = 0;
-let pressForward = 0;
-let pressBack = 0;
-let pressUp = 0;
-let mouseX = 0;
-let mouseY = 0;
-let lock = false;
-
-container.onclick = () => {
-  if (canLock) container.requestPointerLock();
-};
-
-//if the key is pressed
-document.addEventListener("keydown", (e) => {
-  console.log("clicked", e.key);
-  switch (e.key) {
-    case "w":
-    case "ArrowUp":
-      if (lock) pressForward = 1;
-      break;
-    case "s":
-    case "ArrowDown":
-      if (lock) pressBack = 1;
-      break;
-    case "d":
-    case "ArrowRight":
-      if (lock) pressRight = 1;
-      break;
-    case "a":
-    case "ArrowLeft":
-      if (lock) pressLeft = 1;
-      break;
-    case " ":
-      if (lock) pressUp = 1;
-      break;
-    default:
-  }
-});
-
-//if the key is released
-document.addEventListener("keyup", (e) => {
-  switch (e.key) {
-    case "w":
-    case "ArrowUp":
-      pressForward = 0;
-      break;
-    case "s":
-    case "ArrowDown":
-      pressBack = 0;
-      break;
-    case "d":
-    case "ArrowRight":
-      pressRight = 0;
-      break;
-    case "a":
-    case "ArrowLeft":
-      pressLeft = 0;
-      break;
-    case " ":
-      pressUp = 0;
-      break;
-    default:
-  }
-});
-
-document.addEventListener("pointerlockchange", (e) => {
-  lock = !lock;
-});
-
-//if the mouse is moved
-document.addEventListener("mousemove", (e) => {
-  mouseX = e.movementX;
-  mouseY = e.movementY;
-});
-
 function createNewWorld() {
   createSquares(map, "map");
   createSquares(coins, "coin");
@@ -113,8 +44,6 @@ function createNewWorld() {
 createNewWorld();
 
 function createSquares(map, type) {
-  world.style.backgroundImage = "background.webp";
-
   for (let i = 0; i < map.length; i++) {
     const [x, y, z, rx, ry, rz, w, h, color, texture] = map[i];
     const newElement = document.createElement("div");
@@ -157,43 +86,8 @@ function itemCollectDetector(squares, type) {
   }
 }
 
-function update() {
-  //count movement
-  const dx =
-    Math.cos(pawn.ry * deg) * (pressRight - pressLeft) -
-    Math.sin(pawn.ry * deg) * (pressForward - pressBack) * 2;
-
-  const dz =
-    -(
-      Math.sin(pawn.ry * deg) * (pressRight - pressLeft) +
-      Math.cos(pawn.ry * deg) * (pressForward - pressBack)
-    ) * 2;
-
-  const dy = -pressUp * 5;
-
-  const drx = mouseY / 4;
-  const dry = -mouseX / 4;
-
-  //add movement to the coordinates
-  pawn.x = pawn.x + dx;
-  pawn.y = Math.max(Math.min(pawn.y + dy + 2, 0), -150);
-  pawn.z = pawn.z + dz;
-  if (lock) {
-    pawn.rx = pawn.rx + drx;
-    pawn.ry = pawn.ry + dry;
-  }
-
-  //change coordinate of the world
-  world.style.transform =
-    `translateZ(600px) ` +
-    `rotateX(${-pawn.rx}deg) ` +
-    `rotateY(${-pawn.ry}deg) ` +
-    `translate3d(${-pawn.x}px,${-pawn.y}px,${-pawn.z}px)`;
-
-  mouseX = 0;
-  mouseY = 0;
+const interwals = setInterval(() => {
+  pawn.updateMovement();
   itemCollectDetector(coins, "coin");
   itemCollectDetector(keys, "key");
-}
-
-const TimerGame = setInterval(update, 10);
+}, 10);
